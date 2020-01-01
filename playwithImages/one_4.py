a0ddd0c934b65a7fd98c7eca4890182a3f7bc265
Piyush <34549904+Pieter33@users.noreply.github.com>
2020-01-02 02:01:41 +0530

    Update one_4.py

--- playwithImages/one_4.py
+++ playwithImages/one_4.py
@@ -1,9 +1,10 @@
+#mixing images
 import cv2
 import numpy as np
 
 # 500 x 250
-img1 = cv2.imread('a8.jpg')
-img2 = cv2.imread('a10.jpg')
+img1 = cv2.imread('give path to image with extension')
+img2 = cv2.imread('give path to image with extension')
 
 #add = cv2.add(img1,img2)
 add = cv2.addWeighted(img1,0.6,img2,0.4,0.9)
