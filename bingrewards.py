3a023906aca1775cb0ade63ddc4ff02a1325c8af
Piyush <34549904+piyushm-dt@users.noreply.github.com>
2022-11-02 17:43:02 +0530

    Added bingrewards.py
    add

---
+++ bingrewards.py
@@ -0,0 +1,19 @@
+import string
+import random
+import webbrowser
+
+lowercase_letters = string.ascii_lowercase
+def lowercase_word():
+    word = ''
+    random_word_length = random.randint(1,10)
+    while len(word) != random_word_length:
+        word += random.choice(lowercase_letters)
+    return word #Returns the word
+
+
+random_word = lowercase_word()
+
+for i in range(30):
+    random_word = lowercase_word()
+    webbrowser.open('https://www.bing.com/search?q='+random_word)
+
